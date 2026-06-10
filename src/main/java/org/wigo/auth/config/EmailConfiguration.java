8c7c311fe802557eb46c19657bdaa03991149946
Andrew.M <mugisaandrew635@gmail.com>
2026-06-09 22:31:17 -0400

    Add library files

---
+++ src/main/java/org/wigo/auth/config/EmailConfiguration.java
@@ -0,0 +1,45 @@
+package org.wigo.auth.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class EmailConfiguration {
+
+    @Value("${spring.mail.host}")
+    private String mailSenderServer;
+
+    @Value("${spring.mail.port:587}")
+    private int mailSenderPort;
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable:true}")
+    private boolean mailUseTls;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(mailSenderServer);
+        mailSender.setPort(mailSenderPort);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", String.valueOf(mailUseTls));
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
