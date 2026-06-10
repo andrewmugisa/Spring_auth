3fda00fe162d8b8e78cfd6e1dc07e326c24c3ceb
Andrew.M <mugisaandrew635@gmail.com>
2026-06-09 22:31:29 -0400

    Add library files

---
+++ src/main/java/org/wigo/auth/service/EmailService.java
@@ -0,0 +1,24 @@
+package org.wigo.auth.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendVerificationEmail(String receiver, String subject, String message) throws MessagingException {
+        MimeMessage mimeMessage = emailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+        helper.setTo(receiver);
+        helper.setSubject(subject);
+        helper.setText(message, true);
+        emailSender.send(mimeMessage);
+    }
+}
